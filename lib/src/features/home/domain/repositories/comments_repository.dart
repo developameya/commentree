5598daa3f812cbe82c16c6961939dd1e3024c4b6
Ameya Bhagat <developameya@outlook.com>
2023-10-13 10:47:13 +0530

    update fetch comments use case for pagination.

--- lib/src/features/home/domain/repositories/comments_repository.dart
+++ lib/src/features/home/domain/repositories/comments_repository.dart
@@ -2,6 +2,12 @@ import 'package:commentree/src/core/utils/error/failures.dart';
 import 'package:commentree/src/features/home/domain/entities/comment_entity.dart';
 import 'package:dartz/dartz.dart';
 
+///Queries the datasource for the list of comments.
+///
 abstract class CommentsRepository {
-  Future<Either<Failure, List<CommentEntity>>> fetchComments();
+  ///On successful query provides the list of comments as [CommentEntity]
+  ///or returns a [Failure],
+  ///
+  Future<Either<Failure, List<CommentEntity>>> fetchComments(
+      {required int page, required int count});
 }
