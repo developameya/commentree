abc0f6b411382baf514d9a6d976f16ae65e0ac7f
Ameya Bhagat <developameya@outlook.com>
2023-10-13 12:53:32 +0530

    initiate hydrated storage in main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:commentree/src/app.dart';
 import 'package:commentree/src/core/common/services/service_locator/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
   runApp(const MainApp());
 }
